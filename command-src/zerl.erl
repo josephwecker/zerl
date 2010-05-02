13e3c4a444ba0fefb3ab55a49d7a77820d32959d
Joseph Wecker <joseph@spreezio.com>
2010-05-01 21:14:04 -0700

    Starting to put together the interface.

--- command-src/zerl.erl
+++ command-src/zerl.erl
@@ -2,5 +2,29 @@
 
 -export([main/1]).
 
-main(Opts) ->
-  io:format(zerl_parse_opts:parse(Opts)).
+main([Command | Rest]) ->
+  command(list_to_atom(Command), Rest);
+main(_) ->
+  usage().
+
+command(init, [Name | Opts]) ->
+  io:format("Creating new project ~s...\n", [Name]);
+
+command(_, _) ->
+  usage().
+
+
+usage() ->
+  io:format(standard_error,
+    "Usage:
+    init           ProjectName
+    new view       ViewName
+    new controller ControllerName
+    new model      ModelEntityName
+    new behavior   StoryName
+    server         [start | console | stop] [Environment=development]
+      (console connects to running server if there is one)
+    command        [Environment=development] Command [Opts...]
+    help           [Command]
+", []),
+  halt(1).
