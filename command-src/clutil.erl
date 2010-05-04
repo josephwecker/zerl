185799fd24bbece0033cbcc392f0739c030cbab8
Joseph Wecker <jwecker@99-204-138-100.pools.spcsdns.net>
2010-05-03 20:10:02 -0700

    Moving and creating some generic command-line utilities (hence clutil)

---
+++ command-src/clutil.erl
@@ -0,0 +1,74 @@
+-module(clutil).
+
+-export([
+  cancel/0,
+  stat/1, stat/2,
+  estat/1, estat/2,
+  abort/1, abort/2, abort/3,
+  make_dirp/1,
+  promptyn/3, promptc/2
+  ]).
+
+cancel() ->
+  estat("Canceled."),
+  halt(2).
+
+stat(Txt) ->
+  o_stat(standard_io, Txt, []).
+stat(Txt, Args) ->
+  o_stat(standard_io, Txt, Args).
+estat(Txt) ->
+  o_stat(standard_error, Txt, []).
+estat(Txt, Args) ->
+  o_stat(standard_error, Txt, Args).
+
+abort(Msg) ->
+  abort(Msg, []).
+abort(Msg, Args) ->
+  abort(Msg, Args, 100).
+abort(Msg, Args, ErrNumber) ->
+  estat(Msg, Args),
+  halt(ErrNumber).
+
+o_stat(IoDev, Txt, []) ->
+  io:format(IoDev, Txt ++ "\n", []);
+o_stat(IoDev, Txt, [Args]) when not is_list(Args) ->
+  io:format(IoDev, Txt ++ "\n", [Args]);
+o_stat(IoDev, Txt, Args) ->
+  io:format(IoDev, Txt ++ "\n", [Args]).
+
+make_dirp(Dir) ->
+  case filelib:ensure_dir(Dir) of
+    ok ->
+      case file:make_dir(Dir) of
+        ok -> ok;
+        {error, eexist} -> ok;
+        OtherErr -> OtherErr
+      end;
+    OtherErr2 -> OtherErr2
+  end.
+
+promptyn(Prompt, YesFun, NoFun) ->
+  promptc(Prompt,
+    [{"yY", fun(_) -> YesFun() end},
+     {"nN", fun(_) -> NoFun() end}]).
+
+promptc(Prompt, Cases) ->
+  case io:get_chars(Prompt, 1) of
+    eof -> halt(3);
+    {error, Reason} -> io:format(standard_error, "ERROR: ~p", [Reason]);
+    [C] ->
+      case run_c(C, Cases) of
+        '$X-PROMPTS-CHAR-NOT-FOUND' -> promptc(Prompt, Cases);
+        RV -> RV
+      end
+  end.
+
+run_c(_C, []) ->
+  '$X-PROMPTS-CHAR-NOT-FOUND';
+run_c(C, [{LookFor, RunFun} | T]) ->
+  case lists:member(C, LookFor) of
+    true -> RunFun(C);
+    false -> run_c(C, T)
+  end.
+
